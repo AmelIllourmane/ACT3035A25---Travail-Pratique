df08cabf544ca5d974fc4c563b3ac64929064be6
AmelIllourmane <bf291940@ens.uqam.ca>
2025-12-21 00:38:15 -0500

    Affichage des colonnes et sélection variables pertinentes

--- src/02_nettoyage.R
+++ src/02_nettoyage.R
@@ -6,8 +6,30 @@
 #---CODE---
 # Chargement de la librairie nécessaire 
 Library(tidyverse)
+
 #Lecture des données brutes 
 collisions_raw <- read_csv("data/raw/collisions_routieres.csv")
 View(collisions_raw)
+
+# Sélection des variables pertinentes
+names(collisions_raw)
+vars_selectionnees <- c(
+    # Variable réponse
+  "GRAVITE",
+    # Temps
+  "AN",
+  "HEURE_ACCDN",
+    # Environnement
+  "CD_COND_METEO",
+  "CD_ETAT_SURFC",
+    # Route
+  "VITESSE_AUTOR",
+  "CD_CATEG_ROUTE",
+    # Exposition
+  "NB_VEH_IMPLIQUES_ACCDN",
+    # Véhicules
+  "nb_automobile_camion_leger",
+  "nb_motocyclette"
+)
 
 
