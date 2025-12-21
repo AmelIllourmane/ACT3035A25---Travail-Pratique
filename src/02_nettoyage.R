aaa45ae4c212c39647c8ea3d66de97d899e4eb34
AmelIllourmane <bf291940@ens.uqam.ca>
2025-12-21 00:21:16 -0500

    Update 02_nettoyage.R

--- src/02_nettoyage.R
+++ src/02_nettoyage.R
@@ -1,3 +1,3 @@
-# ---Objectif 
+# ---Description
 # Ce script effectue le nettoyage et la préparation des données de collisions routières en vue de l’analyse exploratoire et
 # de la modélisation actuarielle.
