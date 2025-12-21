c7ba2284b736c1ba6f263647644ff61bb58b6006
AmelIllourmane <bf291940@ens.uqam.ca>
2025-12-21 00:24:06 -0500

    Chargement de la librairie tidyverse pour le nettoyage

--- src/02_nettoyage.R
+++ src/02_nettoyage.R
@@ -1,3 +1,8 @@
 # ---Description
 # Ce script effectue le nettoyage et la préparation des données de collisions routières en vue de l’analyse exploratoire et
 # de la modélisation actuarielle.
+
+
+#---CODE---
+# Chargement de la librairie nécessaire 
+Library(tidyverse)
