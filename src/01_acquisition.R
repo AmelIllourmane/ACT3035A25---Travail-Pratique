c21b085a52483ef724cbdafde7be7835fee12b16
AmelIllourmane <bf291940@ens.uqam.ca>
2025-12-20 23:46:09 -0500

    Documentation du script 01_acquisition.R

--- src/01_acquisition.R
+++ src/01_acquisition.R
@@ -1,1 +1,14 @@
 
+# Description  
+Ce script permet d'effectuer l'acquisition des données utilisées dans le cadre du projet. 
+Les données proviennent du portail Données Québec et concernent les collisions routières sur le territoire de la ville de Montréal.
+
+# Accès aux données 
+Les données sont accessibles publiquement à l'adresse suivante:
+https://www.donneesquebec.ca/recherche/dataset/vmtl-collisions-routieres/resource/05deae93-d9fc-4acb-9779-e0942b5e962f
+En raison de la taille importante du fichier, les données brutes ne sont pas incluses directement dans le dépot GitHub. 
+Le fichier CSV doit être téléchargé manuellement et placé dans le dossier data/raw/ avant l'éxécution de ce script.
+
+# Objectif de ce script
+- Importer les données brutes
+- Effectuer une première inspection de la strcution des données 
