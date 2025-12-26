54cf3bc26af3021eb4e4146f3856aa1ee22b9a41
AmelIllourmane <bf291940@ens.uqam.ca>
2025-12-26 02:56:27 -0500

    Description du script d'analyse exploratoire

--- src/03_exploration.R
+++ src/03_exploration.R
@@ -1,1 +1,4 @@
-
+#---Description:
+# Ce script réalise l'analyse exploratoire des données de collisions routières nettoyées.
+# Il vise à décrire les principales caractéristiques des variables, à analyser les distributions , à produire des statistiques
+# descriptives et des visualisations afin de mieux comprendre la structure des données avant la modélisation actuarielle. 
