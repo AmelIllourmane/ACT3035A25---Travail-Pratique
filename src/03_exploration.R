a3188e00703a36867471bf12be6d2d67ca11d89f
AmelIllourmane <bf291940@ens.uqam.ca>
2025-12-26 16:21:44 -0500

    Mesures de tendance centrale et de dispersion pour la vitesse et le nombre de vehiculés impliqués dans les accidents

--- src/03_exploration.R
+++ src/03_exploration.R
@@ -13,7 +13,7 @@ collisions_clean <- read_csv("data/processed/collisions_nettoyees.csv")
 dim(collisions_clean)
 str(collisions_clean)
 
-# Statistiques descriptives des variables numériques
+# Statistiques descriptives des variables numériques 
 collisions_clean %>%
   select(
     VITESSE_AUTOR,
@@ -22,4 +22,29 @@ collisions_clean %>%
     nb_motocyclette
   ) %>%
   summary()
+ 
+# Mesures de tendance centrale et de dispersion pour la vitesse et le nombre de vehiculés impliqués dans les accidents 
+collisions_clean %>%
+  summarise(
+    moyenne_vitesse = mean(VITESSE_AUTOR, na.rm = TRUE),
+    mediane_vitesse = median(VITESSE_AUTOR, na.rm = TRUE),
+    ecart_type_vitesse = sd(VITESSE_AUTOR, na.rm = TRUE),
+    
+    moyenne_nb_veh = mean(NB_VEH_IMPLIQUES_ACCDN, na.rm = TRUE),
+    mediane_nb_veh = median(NB_VEH_IMPLIQUES_ACCDN, na.rm = TRUE),
+    ecart_type_nb_veh = sd(NB_VEH_IMPLIQUES_ACCDN, na.rm = TRUE)
+  )
+
+# 
+
+
+
+
+
+
+
+
+
+
+
 
