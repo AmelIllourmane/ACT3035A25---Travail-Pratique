ab6c380de0b104efc8eedf9e35cc5ba6eacdb4bc
AmelIllourmane <bf291940@ens.uqam.ca>
2025-12-26 03:00:08 -0500

    Chargement et lecture des données nettoyées

--- src/03_exploration.R
+++ src/03_exploration.R
@@ -1,4 +1,16 @@
-#---Description:
+# ---Description
 # Ce script réalise l'analyse exploratoire des données de collisions routières nettoyées.
 # Il vise à décrire les principales caractéristiques des variables, à analyser les distributions , à produire des statistiques
 # descriptives et des visualisations afin de mieux comprendre la structure des données avant la modélisation actuarielle. 
+
+
+#---CODE---
+# Chargement des données nettoyées
+
+library(tidyverse)
+collisions_clean <- read_csv("data/processed/collisions_nettoyees.csv")
+
+dim(collisions_clean)
+str(collisions_clean)
+
+# 
