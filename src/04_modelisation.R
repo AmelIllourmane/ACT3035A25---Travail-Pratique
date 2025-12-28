fe65bf31fb4e7dc1f35e2aae37408c2b802d3e2f
AmelIllourmane <bf291940@ens.uqam.ca>
2025-12-27 23:43:08 -0500

    Update  description 04_modelisation.R

--- src/04_modelisation.R
+++ src/04_modelisation.R
@@ -1,3 +1,5 @@
 #---Description
 # Ce script vise à modéliser la gravité des collisions routières à partir des données nettoyées.
-# Il utilise deux modèles actuariels: la régression logistique ainsi que le modèle linéaire généralisé (GLM) 
+# Il utilise deux modèles actuariels: 
+    # 1. La régression logistique 
+    # 2. Le modèle linéaire généralisé (GLM) 
