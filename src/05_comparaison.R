fb588ff2cf97ef78324a70bd7c3ee3de68fe79a3
AmelIllourmane <bf291940@ens.uqam.ca>
2025-12-28 02:01:03 -0500

    Update 05_comparaison.R

--- src/05_comparaison.R
+++ src/05_comparaison.R
@@ -3,4 +3,3 @@ summary(modele_lineaire)
 summary(modele_logistique)
 
 # Le modèle linéaire présente des limites importantes puisque les prédictions ne sont pas contraintes entre 0 et 1, ce qui complique l’interprétation.
-# La régression logistique fournit des probabilités cohérentes.
