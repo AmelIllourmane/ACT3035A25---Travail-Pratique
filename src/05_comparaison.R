146a6dfeb6729cfdf66d416e388eb7ec6d81be9f
AmelIllourmane <bf291940@ens.uqam.ca>
2025-12-28 22:57:55 -0500

    Update 05_comparaison.R

--- src/05_comparaison.R
+++ src/05_comparaison.R
@@ -3,3 +3,4 @@ summary(modele_lineaire)
 summary(modele_logistique)
 
 # Le modèle linéaire présente des limites importantes puisque les prédictions ne sont pas contraintes entre 0 et 1, ce qui complique l’interprétation.
+
